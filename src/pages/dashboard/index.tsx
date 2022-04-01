60779d5cf4eb322bdad1c1260e925012282e1e2d
zhangxinrong <784548253@qq.com>
2022-03-31 22:43:26 -0400

    add signin and signup ui

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -14,21 +14,29 @@ import Management from './management';
 import Profile from './profile';
 import CreateGroup from './create';
 import ChatRoom from './chatroom';
+import SignIn from "./signin";
+import SignUp from "./signup";
 
 export default function Dashboard() {
     const [tag, setTag] = useState('explore');
+    const [signing, setSigning] = useState(true);
     const navigate = useNavigate();
     const location = useLocation();
 
     const matches = matchRoutes([{ path: '/dashboard/:tag' }], location);
     const urlTag = matches?.[0].params.tag || '';
     const withInDashboardPage = urlTag && 'explore/management/profile'.includes(urlTag);
-    
+
     useEffect(() => {
+        if (urlTag != 'signin' && urlTag != 'signup') {
+            setSigning(false);
+        } else {
+            setSigning(true);
+        }
         if (urlTag !== tag) {
             setTag(urlTag);
         }
-    // eslint-disable-next-line
+        // eslint-disable-next-line
     }, [urlTag]);
 
     const changeHandler = (newTag: string) => {
@@ -48,21 +56,33 @@ export default function Dashboard() {
             }}>
                 <Routes>
                     <Route path="/explore" element={<Explore />} />
+                    <Route path="/signin" element={<SignIn />} />
+                    <Route path="/signup" element={<SignUp />} />
                     <Route path="/management" element={<Management />} />
                     <Route path="/profile" element={<Profile />} />
                     <Route path="/create" element={<CreateGroup />} />
                     <Route path="/chat/*" element={<ChatRoom />} />
                 </Routes>
             </div>
-            <BottomNavigation currentActive={tag} changeHandler={changeHandler} />
+
+            <div>
+                {
+                    !signing ?
+                        <BottomNavigation currentActive={tag} changeHandler={changeHandler} />
+                        :
+                        null
+                }
+            </div>
+
+
         </BaseContainer>
         {
             withInDashboardPage ?
-            (<Box sx={{ position: 'fixed', right: 30, bottom: 80, zIndex: 2 }}>
-                <Fab color="primary" aria-label="create group" component={Link} to="/dashboard/create">
-                    <AddIcon />
-                </Fab>
-            </Box>) : null
+                (<Box sx={{ position: 'fixed', right: 30, bottom: 80, zIndex: 2 }}>
+                    <Fab color="primary" aria-label="create group" component={Link} to="/dashboard/create">
+                        <AddIcon />
+                    </Fab>
+                </Box>) : null
         }
     </>)
 }
--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -6,6 +6,7 @@ import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom';
 
 import BaseContainer from '../../components/baseContainer';
+import {Stack} from "@mui/material";
 
 export default function Landing() {
     return (<BaseContainer style={{ background: 'black' }}>
@@ -14,8 +15,21 @@ export default function Landing() {
             <Box sx={{ fontSize: 'h3.fontSize', m: 1, color: 'white', fontFamily: 'Ubuntu' }}>FoodieNet</Box>
             </Typography>
         </Box>
-        <Box component="span" sx={{ display: 'flex', p: 2, marginTop: 25, width: '100%', justifyContent: 'center' }}>
-            <Button sx={{ border: '1px dashed grey', padding: '10px 28px' }} component={Link} to="/dashboard/explore">Explore Now</Button>
+
+        <Box component="span" sx={{ display: 'flex', p: 2, marginTop: 35, width: '100%', justifyContent: 'center' }}>
+            <Stack
+                sx={{ pt: 4 }}
+                direction="row"
+                spacing={4}
+                justifyContent="flex-end"
+            >
+                <Button sx={{ border: '1px dashed grey', padding: '10px 28px' }} component={Link} to="/dashboard/explore">Explore Now</Button>
+                <Button sx={{ border: '1px dashed grey', padding: '10px 28px' }} component={Link} to="/dashboard/signin">Sign In</Button>
+                <Button sx={{ border: '1px dashed grey', padding: '10px 28px' }} component={Link} to="/dashboard/signup">Sign Up</Button>
+            </Stack>
         </Box>
+
+
+
     </BaseContainer>);
 }
